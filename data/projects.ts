f23de02bc53d8cf9f7992c8e70714c1ac4a6fd1f
fendioy <fendi.amorokhman@oyindonesia.com>
2023-06-03 16:00:38 +0700

    update

--- data/projects.ts
+++ data/projects.ts
@@ -2,7 +2,7 @@ export const dataProjects = [
   {
     "name": "Machine Translation Kailinese",
     "stacks": ["streamlit","mysql", "python", "transformer", "pytorch", "idcloudhost"],
-    "description": "Machine translation (NLP) platform for the Indonesian-Kailinese language, utilizing the pretrained IndoBART-V2 model based on the BART model",
+    "description": "Machine translation (NLP) platform for the Indonesian-Kailinese language, utilizing the pretrained IndoBART-V2 model based on the BART model. The Kailinese language is a local language that has flourished and evolved in the Central Sulawesi Province of Indonesia. This project is a part of my undergraduate thesis, conducted under the guidance of my advisors, Dr. Ade Romadhony, S.T., M.T. and Dr. Aditya Firman Ihsan, S.Si., M.Si., and generously funded by Telkom University.",
     "picture": "/projects/translate_kaili.svg",
     "date": "May 2023",
     "link_project": "https://translate.kailidev.com/"
