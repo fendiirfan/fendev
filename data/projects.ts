edb1a2f1e136e48a3c0b8ccd978d15227e3525e7
fendioy <fendi.amorokhman@oyindonesia.com>
2023-06-04 17:10:32 +0700

    ITE Law project

--- data/projects.ts
+++ data/projects.ts
@@ -1,11 +1,19 @@
 export const dataProjects = [
   {
     "name": "Machine Translation Kailinese",
-    "stacks": ["streamlit","mysql", "python", "transformer", "pytorch", "idcloudhost"],
+    "stacks": ["streamlit","mysql", "python", "transformer", "pytorch", "fastapi", "idcloudhost"],
     "description": "Machine translation (NLP) platform for the Indonesian-Kailinese language, utilizing the pretrained IndoBART-V2 model based on the BART model. The Kailinese language is a local language that has flourished and evolved in the Central Sulawesi Province of Indonesia. This project is a part of my undergraduate thesis, conducted under the guidance of my advisors, Dr. Ade Romadhony, S.T., M.T. and Dr. Aditya Firman Ihsan, S.Si., M.Si., and generously funded by Telkom University.",
     "picture": "/projects/translate_kaili.svg",
     "date": "May 2023",
     "link_project": "https://translate.kailidev.com/"
+  },
+  {
+    "name": "ITE Law Violation Detector Web Application",
+    "stacks": ["python","streamlit", "tensorflow"],
+    "description": "ITE Law Violation Detector Web Application is a powerful tool developed using Python, Streamlit, and TensorFlow, designed to identify and flag any violations of the Information and Electronic Transactions (ITE) Law.",
+    "picture": "/projects/ITE Detector.webp",
+    "date": "January 2022",
+    "link_project": "http://ite-detector.fendev.my.id:8503/"
   },
   {
     "name": "Antriin Web Application",
@@ -15,4 +23,5 @@ export const dataProjects = [
     "date": "December 2021",
     "link_project": "https://antriin.vercel.app"
   },
+  
 ]
